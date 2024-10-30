6fda24b33431786eb72fa7ed87c576db7c3f854c
Sascha Lisson <sascha.lisson@itemis.de>
2024-10-30 14:18:27 +0100

    build: passing MPS_VERSION didn't work

--- docker-publish.sh
+++ docker-publish.sh
@@ -2,11 +2,9 @@
 
 set -e
 set -x
+set -u
 
 cd "$(dirname "$0")"
-
-# Default value for MPS_VERSION
-MPS_VERSION="${MPS_VERSION:=2023.2.2}"
 
 MPS_MAJOR_VERSION=`echo $MPS_VERSION | grep -oE '20[0-9]{2}\.[0-9]+'`
 IMAGE_VERSION="$(./version.sh)"
