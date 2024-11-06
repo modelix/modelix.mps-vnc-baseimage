f62cdbac7d87e216e39f1a94b2670bbd36252eb1
Sascha Lisson <sascha.lisson@itemis.de>
2024-11-06 16:43:30 +0100

    feat: find MPS projects also in root directories

--- src/pre-startup.sh
+++ src/pre-startup.sh
@@ -3,15 +3,17 @@
 # This script is executed inside an XTERM window before starting MPS
 
 set -x
-set -e
 
 # Download and run shell script provided in $PRE_STARTUP_SCRIPT_URL
+echo "PRE_STARTUP_SCRIPT_URL=$PRE_STARTUP_SCRIPT_URL"
 if [ -n "$PRE_STARTUP_SCRIPT_URL" ]; then
   mkdir -p /tmp/prestartup
   cd /tmp/prestartup
   curl -o /tmp/prestartup/script.sh "$PRE_STARTUP_SCRIPT_URL"
   chmod +x /tmp/prestartup/script.sh
   /tmp/prestartup/script.sh
+else
+  echo "Not downloading any pre-startup script"
 fi
 
 # Let MPS open all projects in $HOME/mps-projects
@@ -20,7 +22,35 @@ echo '<application>' > $RECENT_PROJECTS_FILE
 echo '  <component name="RecentProjectsManager">' >> $RECENT_PROJECTS_FILE
 echo '    <option name="additionalInfo">' >> $RECENT_PROJECTS_FILE
 echo '      <map>' >> $RECENT_PROJECTS_FILE
-find /config/home/mps-projects -type d -name "*.mps" | while read -r dir; do
+
+find / -type d -name "*.mps" \
+    -not -path "/bin/*" \
+    -not -path "/boot/*" \
+    -not -path "/defaults/*" \
+    -not -path "/dev/*" \
+    -not -path "/etc/*" \
+    -not -path "/init/*" \
+    -not -path "/lib/*" \
+    -not -path "/lib64/*" \
+    -not -path "/media/*" \
+    -not -path "/mnt/*" \
+    -not -path "/mps/*" \
+    -not -path "/mps-config/*" \
+    -not -path "/mps-languages/*" \
+    -not -path "/mps-plugins/*" \
+    -not -path "/mps-projects/*" \
+    -not -path "/mps-user-home/*" \
+    -not -path "/opt/*" \
+    -not -path "/proc/*" \
+    -not -path "/root/*" \
+    -not -path "/run/*" \
+    -not -path "/sbin/*" \
+    -not -path "/srv/*" \
+    -not -path "/sys/*" \
+    -not -path "/tmp/*" \
+    -not -path "/usr/*" \
+    -not -path "/var/*" | while read -r dir
+do
   PROJECT_DIR="$(dirname "$dir")"
   echo "        <entry key=\"$PROJECT_DIR\">" >> $RECENT_PROJECTS_FILE
   echo '          <value>' >> $RECENT_PROJECTS_FILE
--- src/run-indexer.sh
+++ src/run-indexer.sh
@@ -3,9 +3,34 @@
 # Indexes all MPS projects to speedup the startup
 
 set -x
-set -e
 
-find /config/home/mps-projects -type d -name ".mps" | while read -r dir
+find / -type d -name "*.mps" \
+    -not -path "/bin/*" \
+    -not -path "/boot/*" \
+    -not -path "/defaults/*" \
+    -not -path "/dev/*" \
+    -not -path "/etc/*" \
+    -not -path "/init/*" \
+    -not -path "/lib/*" \
+    -not -path "/lib64/*" \
+    -not -path "/media/*" \
+    -not -path "/mnt/*" \
+    -not -path "/mps/*" \
+    -not -path "/mps-config/*" \
+    -not -path "/mps-languages/*" \
+    -not -path "/mps-plugins/*" \
+    -not -path "/mps-projects/*" \
+    -not -path "/mps-user-home/*" \
+    -not -path "/opt/*" \
+    -not -path "/proc/*" \
+    -not -path "/root/*" \
+    -not -path "/run/*" \
+    -not -path "/sbin/*" \
+    -not -path "/srv/*" \
+    -not -path "/sys/*" \
+    -not -path "/tmp/*" \
+    -not -path "/usr/*" \
+    -not -path "/var/*" | while read -r dir
 do
   /mps/bin/mps.sh warmup --project-dir="$(dirname "$dir")"
 done
