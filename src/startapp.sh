c7651cc45bb5c92582f6cf1d757f8df2013d73e3
Sascha Lisson <sascha.lisson@itemis.de>
2024-11-06 16:44:13 +0100

    fix: cleaner xterm output

--- src/startapp.sh
+++ src/startapp.sh
@@ -3,9 +3,13 @@
 # This script is executed by the container to start the GUI application.
 
 set -x
-set -e
 
-xterm -e "mkdir -p /tmp/startup ; cd /tmp/startup ; /pre-startup.sh"
+mkdir -p /tmp/startup
+(
+  cd /tmp/startup
+  #/pre-startup.sh
+  xterm -e "/pre-startup.sh"
+)
 
 # Run MPS
 /mps/bin/mps.sh
