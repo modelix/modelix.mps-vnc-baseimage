252cd063cfb33f6d6af95c90bff55c61340f83c3
slisson <sl@q60.de>
2024-11-27 09:52:04 +0100

    fix: skip index warmup for versions before 2022.2
    It's just not supported.

--- src/run-indexer.sh
+++ src/run-indexer.sh
@@ -4,33 +4,40 @@
 
 set -x
 
-find / -type d -name ".mps" \
-    -not -path "/bin/*" \
-    -not -path "/boot/*" \
-    -not -path "/defaults/*" \
-    -not -path "/dev/*" \
-    -not -path "/etc/*" \
-    -not -path "/init/*" \
-    -not -path "/lib/*" \
-    -not -path "/lib64/*" \
-    -not -path "/media/*" \
-    -not -path "/mnt/*" \
-    -not -path "/mps/*" \
-    -not -path "/mps-config/*" \
-    -not -path "/mps-languages/*" \
-    -not -path "/mps-plugins/*" \
-    -not -path "/mps-projects/*" \
-    -not -path "/mps-user-home/*" \
-    -not -path "/opt/*" \
-    -not -path "/proc/*" \
-    -not -path "/root/*" \
-    -not -path "/run/*" \
-    -not -path "/sbin/*" \
-    -not -path "/srv/*" \
-    -not -path "/sys/*" \
-    -not -path "/tmp/*" \
-    -not -path "/usr/*" \
-    -not -path "/var/*" | while read -r dir
-do
-  /mps/bin/mps.sh warmup --project-dir="$(dirname "$dir")"
-done
+MPS_MAJOR_VERSION=$(grep "mpsBootstrapCore.version=" < "/mps/build.properties" | cut -d'=' -f2)
+
+if [[ MPS_MAJOR_VERSION < "2022.2" ]]
+then
+  echo "MPS version doesn't support index warmup: $MPS_MAJOR_VERSION"
+else
+  find / -type d -name ".mps" \
+      -not -path "/bin/*" \
+      -not -path "/boot/*" \
+      -not -path "/defaults/*" \
+      -not -path "/dev/*" \
+      -not -path "/etc/*" \
+      -not -path "/init/*" \
+      -not -path "/lib/*" \
+      -not -path "/lib64/*" \
+      -not -path "/media/*" \
+      -not -path "/mnt/*" \
+      -not -path "/mps/*" \
+      -not -path "/mps-config/*" \
+      -not -path "/mps-languages/*" \
+      -not -path "/mps-plugins/*" \
+      -not -path "/mps-projects/*" \
+      -not -path "/mps-user-home/*" \
+      -not -path "/opt/*" \
+      -not -path "/proc/*" \
+      -not -path "/root/*" \
+      -not -path "/run/*" \
+      -not -path "/sbin/*" \
+      -not -path "/srv/*" \
+      -not -path "/sys/*" \
+      -not -path "/tmp/*" \
+      -not -path "/usr/*" \
+      -not -path "/var/*" | while read -r dir
+  do
+    /mps/bin/mps.sh warmup --project-dir="$(dirname "$dir")"
+  done
+fi
